6cff5c82e7c57a0abfc40ebc3ce9a249bf864d53
YayCover9000 <yassuanf@googlemail.com>
2023-02-20 19:31:33 +0100

    ExerciseOne.java Prints now as i want it

--- src/ExerciseOne.java
+++ src/ExerciseOne.java
@@ -1,2 +1,16 @@
 public class ExerciseOne {
+    public static void ExerciseOne() {
+        int a = 9;
+        int b = 0;
+        int array[] = new int[10];
+        while (a > -1) {
+            //System.out.println(a);
+            array[b] = a;
+            a--;
+            b++;
+        }
+        for(int element : array) {
+            System.out.print(element + " ");
+        }
+    }
 }
