4d78c7fcd01cda7fe11389ae9ba7e82d147343a8
YayCover9000 <yassuanf@googlemail.com>
2023-02-22 17:59:50 +0100

    deletet CoffeeTypeComp

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,5 @@ public class Main {
         //ExerciseTwo.ExerciseTwo();
         //ExerciseThree.ExerciseThree();
         //ExerciseFour.ExerciseFour();
-        CoffeeComperator.CoffeeComperator();
     }
 }
