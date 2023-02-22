a3793cbd5d16fcd6cf82f421e7b73c278e13b655
YayCover9000 <yassuanf@googlemail.com>
2023-02-22 13:08:15 +0100

    added ExerciseThree.java

---
+++ src/ExerciseThree.java
@@ -0,0 +1,6 @@
+public class ExerciseThree {
+    public static void ExerciseThree() {
+        int a = -1;
+        System.out.print(1 - a++);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //ExerciseOne.ExerciseOne();
-        ExerciseTwo.ExerciseTwo();
+       //ExerciseTwo.ExerciseTwo();
+        ExerciseThree.ExerciseThree();
     }
 }
