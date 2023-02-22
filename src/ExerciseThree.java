6361e09340f6e4adf80eee885726dc699dbf5182
YayCover9000 <yassuanf@googlemail.com>
2023-02-22 13:14:14 +0100

    ExerciseThree.java wurde verstanden und commentiert

--- src/ExerciseThree.java
+++ src/ExerciseThree.java
@@ -1,6 +1,11 @@
 public class ExerciseThree {
     public static void ExerciseThree() {
         int a = -1;
-        System.out.print(1 - a++);
+        int b = a++;
+        int c = b;
+        System.out.println(b);
+        System.out.println(1 - b);
+        System.out.println(c);
+        System.out.print(1 - a++); //1 -1 = 2 weil der Compiler rechnet: 1-(-1) = 2 // und - plus - ist + ergo 1+1!!
     }
 }
