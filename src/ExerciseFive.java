7190e9f87de52911f6a63c97bb307b2d7157279d
YayCover9000 <yassuanf@googlemail.com>
2023-02-24 16:54:21 +0100

    less compact but more understanable

--- src/ExerciseFive.java
+++ src/ExerciseFive.java
@@ -9,9 +9,11 @@ public class ExerciseFive {
         boolean firstNumSmallerSecNum = secondNumber >= firstNumber;
         boolean firstNumSmallerThirdNum = firstNumber <= thirdNumber;
         boolean firstNumBiggerThirdNum = firstNumber >= thirdNumber;
+        boolean statement1 = firstNumBigerSecNum && firstNumSmallerThirdNum;
+        boolean statment2 = firstNumBiggerThirdNum && firstNumSmallerSecNum;
         boolean output = false;
 
-        if((firstNumBigerSecNum && firstNumSmallerThirdNum) || (firstNumBiggerThirdNum && firstNumSmallerSecNum)){
+        if(statement1 || statment2){
             output = true;
         }
         System.out.print(output);
