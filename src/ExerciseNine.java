ad28ae0f6913edb113f63da0a4ecd20b00905299
YayCover9000 <yassuanf@googlemail.com>
2023-02-26 16:20:03 +0100

    add ExerciseNine.java now does the right thing

--- src/ExerciseNine.java
+++ src/ExerciseNine.java
@@ -4,8 +4,9 @@ public class ExerciseNine {
         Scanner scanner = new Scanner(System.in);
         int firstInput = scanner.nextInt();
         int secondInput = scanner.nextInt();
+        scanner.close();
         int output = 1;
-        for (int i = firstInput; firstInput < secondInput; i++) {
+        for (int i = firstInput; i < secondInput; i++) {
             output *= i;
         }
         System.out.print(output);
