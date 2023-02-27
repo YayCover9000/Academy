6cdf64e6341e35ec9c00f1d962c3fbee6fb23c0b
YayCover9000 <yassuanf@googlemail.com>
2023-02-27 14:14:07 +0100

    add price to it

--- src/ExerciseEleven.java
+++ src/ExerciseEleven.java
@@ -1,3 +1,8 @@
+/**Programm to Solve an Exercise in JetBrainsAcademy
+ * it shall calculate the price for seats in a Cinema
+ *
+ * @autor Yassuan Foljanty Catanzaro
+ * */
 import java.util.Scanner;
 public class ExerciseEleven {
     public static void exerciseEleven() {
@@ -8,21 +13,41 @@ public class ExerciseEleven {
             > 7
             Total income:
             $560 */
+        int value;
+        int price;
+        //accesses evenOrOdd for numRows
         int numRows = scanner.nextInt();
+        value = numRows;
+        ExerciseEleven.evenOrOdd(value);
+        //accesses evenOrOdd for numSeats
         int numSeats = scanner.nextInt();
+        value = numSeats;
+        ExerciseEleven.evenOrOdd(value);
+        //calculates all Seats in room
         int gesSeats = numRows * numSeats;
-        System.out.print(gesSeats);
-        if (gesSeats >= 50) {
-            ges
+        System.out.println(gesSeats);
+
+        if (gesSeats <= 60) {
+            price = 10;
+            System.out.print(price + "€ per Seat");
         }
-        System.out.println("give me a value to determan if odd or not");
-        int value = scanner.nextInt();
+        else if (gesSeats > 60) {
+            System.out.print(" more then 60 Seats");
+        }
+        //System.out.println("give me a value to determan if odd or not");
+        //int value = scanner.nextInt();
+
+
+
+    }
+    private static int evenOrOdd(int value){
         if (isEven(value)) {
             System.out.println(value + " isEven");
         }
         else if (isOdd(value)) {
             System.out.println(value + " isOdd");
         }
+        return value;
     }
     private static boolean isEven(int value) {
         return value % 2 == 0;
