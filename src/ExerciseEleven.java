95876b167832a130aef90772af9351c804fa4fba
YayCover9000 <yassuanf@googlemail.com>
2023-02-27 15:08:47 +0100

    does not work but safety first here

--- src/ExerciseEleven.java
+++ src/ExerciseEleven.java
@@ -4,51 +4,85 @@
  * @autor Yassuan Foljanty Catanzaro
  * */
 import java.util.Scanner;
-public class ExerciseEleven {
+public class ExerciseEleven{
     public static void exerciseEleven() {
         Scanner scanner = new Scanner(System.in);
-        /* Enter the number of rows:
-            > 9
-            Enter the number of seats in each row:
-            > 7
-            Total income:
-            $560 */
+                                                                            /* Enter the number of rows:
+                                                                                > 9
+                                                                                Enter the number of seats in each row:
+                                                                                > 7
+                                                                                Total income:
+                                                                                $560 */
         int value;
         int price;
+        boolean even = false;
         //accesses evenOrOdd for numRows
         int numRows = scanner.nextInt();
-        value = numRows;
-        ExerciseEleven.evenOrOdd(value);
-        //accesses evenOrOdd for numSeats
+                                                                            /* value = numRows;
+                                                                            ExerciseEleven.evenOrOdd(value, even); */
+                                                                            //accesses evenOrOdd for numSeats
         int numSeats = scanner.nextInt();
-        value = numSeats;
-        ExerciseEleven.evenOrOdd(value);
-        //calculates all Seats in room
+                                                                            /* value = numSeats;
+                                                                            ExerciseEleven.evenOrOdd(value, even); */
+                                                                            //calculates all Seats in room
         int gesSeats = numRows * numSeats;
         System.out.println(gesSeats);
+        //checks if gesSeats are Even or Odd
+        value = gesSeats;
+        ExerciseEleven.evenOrOdd(value, even);
+        System.out.println(even);
+        if (!even) {
 
-        if (gesSeats <= 60) {
-            price = 10;
-            System.out.print(price + "€ per Seat");
         }
-        else if (gesSeats > 60) {
-            System.out.print(" more then 60 Seats");
+        else {
+
         }
+
+         //gesSeats smaller 60
+            if (gesSeats <= 60) {
+                price = 10;
+                System.out.print(price + "€ per Seat");
+            }
+                                                                /* case 1: //gesSeats bigger 60
+                                                                    if (gesSeats > 60) {
+                                                                        System.out.println(" more then 60 Seats");
+                                                                        boolean gesSeatsEven = false; */
+                                                                 /*if ( numRows % 2 == 0) { //gesSeats Even
+                                                                            gesSeatsEven = true;
+                                                                        }
+                                                                        if (gesSeatsEven == true) {
+                                                                            int frontPart = numRows / 2;
+                                                                            int frontPartPrice = 10 * frontPart;
+                                                                            System.out.println();
+                                                                        } */
+                   // int totalIncome = frontPartPrice *
+        }
+
+
+            /* //Does not give out remainder
+            numRows = numRows / 2;
+            System.out.println(numRows); */
+
+
         //System.out.println("give me a value to determan if odd or not");
         //int value = scanner.nextInt();
 
 
 
-    }
-    private static int evenOrOdd(int value){
+
+    private static boolean evenOrOdd(int value, boolean even){
+
         if (isEven(value)) {
             System.out.println(value + " isEven");
+            even = true;
         }
         else if (isOdd(value)) {
             System.out.println(value + " isOdd");
+            even = false;
         }
-        return value;
+        return even;
     }
+
     private static boolean isEven(int value) {
         return value % 2 == 0;
     }
@@ -57,3 +91,4 @@ public class ExerciseEleven {
     }
 
 }
+
